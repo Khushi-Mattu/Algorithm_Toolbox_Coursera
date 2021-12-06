3d9bbf5dba8271c4b1ee7ccce70660aa7141245d
Khushi-Mattu <khushi.mattu2020@vitstudent.ac.in>
2021-12-06 14:34:22 +0530

    questions

---
+++ Week3_programming_challenges/collecting_signatures.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(i=0;i<n;i++){
+        scanf("%d %d",&a[i],&b[i]);
+    }
+    int num,cnt;
+    int j=0;
+    
+}
